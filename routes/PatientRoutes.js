5a9e5bcc7d9e0633a26437842b98e9b8881747e4
PremiyaR <rajanipremiya@gmail.com>
2024-11-15 12:23:30 +0530

    initial changes

--- routes/PatientRoutes.js
+++ routes/PatientRoutes.js
@@ -17,20 +17,26 @@ router.post("/", async (req, res) => {
     const aesKeyBuffer = crypto.privateDecrypt(
         {
           key: privateKey,
-          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-          oaepHash: "sha256", // Ensure the same hash as the frontend
+          padding: crypto.constants.RSA_PKCS1_PADDING,
+          // oaepHash: "sha256", // Ensure the same hash as the frontend
         },
         Buffer.from(encryptedAESKey, "base64")
       );
-      
+      console.log('-----------------------------//aesKeyBuffer', aesKeyBuffer)
       // Convert the AES key buffer to UTF-8 string (if it's a string-based AES key)
       const aesKey = aesKeyBuffer.toString("utf8");
+      console.log("Decrypted AES Key (UTF-8):", aesKey);
 
     // Decrypt data with AES key
     const decryptedData = CryptoJS.AES.decrypt(encryptedData, aesKey).toString(
       CryptoJS.enc.Utf8
     );
+    console.log('-----------------------decryptedData::', decryptedData);
+    if (!decryptedData) {
+      return res.status(500).json({ message: `Something went wrong while CryptoJS.AES.decrypt`, decryptedData, aesKey, encryptedAESKey, encryptedData })
+    }
     const patientData = JSON.parse(decryptedData);
+    console.log("Received Encrypted AES Key (Base64):", encryptedAESKey);  
 
     // Save patient data to database
     const patient = new Patient(patientData);
@@ -38,7 +44,7 @@ router.post("/", async (req, res) => {
     res.status(201).json(patient);
   } catch (err) {
     console.error("Error in post call:", err);
-    res.status(400).json({ error: err.message });
+    res.status(400).json({ error: err.message, stack: err.stack });
   }
 });
 
